be3345eeaf194ef5879f5265e443ac91477fb66f
bunnyfirth <bunnyfirth@outlook.com>
2024-06-27 18:02:43 -0400

    Initial commit

---
+++ myrandom.py
@@ -0,0 +1,13 @@
+import random
+
+value = random.random()
+print(value)
+GEORGE = random.uniform(5, 10)
+print (GEORGE)
+Bob = 1
+bob1 = 2
+bob2 = 3
+bob4 = 4
+bob10 = 10
+jane = (bob1+bob2+bob4)
+print (jane+bob10-bob4)
